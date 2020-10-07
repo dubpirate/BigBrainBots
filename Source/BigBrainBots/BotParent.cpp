a2385c93a414689b96772b8bf3a3dc58205b443b
60and80 <hugh.m@me.com>
2020-10-08 12:48:08 +1300

    \#29 Re-bound camera to topdown view

--- Source/BigBrainBots/BotParent.cpp
+++ Source/BigBrainBots/BotParent.cpp
@@ -36,7 +36,7 @@ ABotParent::ABotParent()
 	CameraBoom->SetupAttachment(RootComponent);
 	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
 	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	CameraBoom->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f));
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
